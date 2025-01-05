fa588bbe48e2d9dec790a4e6eda018105fa39959
nao.importa.web <nao.importa.web@gmail.com>
2025-01-05 14:58:43 -0300

    Instalador

--- local/install.sh
+++ local/install.sh
@@ -6,12 +6,14 @@ fi
 
 cp -r ../* /opt/codoencrypt/
 
+sudo apt install cryptsetup -y
+
 echo "[Unit]" > /etc/systemd/system/kfm_codo.service
 echo "Description=Codoencrypt local" >> /etc/systemd/system/kfm_codo.service
 echo "[Service]" >> /etc/systemd/system/kfm_codo.service
 echo "Type=forking" >> /etc/systemd/system/kfm_codo.service
 echo "Environment=SUDO_USER=$SUDO_USER" >> /etc/systemd/system/kfm_codo.service
-echo "ExecStart=/opt/codoencrypt/local/start.sh" >> /etc/systemd/system/kfm_codo.service
+echo "ExecStart=/opt/codoencrypt/local/start.sh &" >> /etc/systemd/system/kfm_codo.service
 echo "[Install]" >> /etc/systemd/system/kfm_codo.service
 echo "WantedBy=graphical.target" >> /etc/systemd/system/kfm_codo.service
 
